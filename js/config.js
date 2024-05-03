f84c89a63fdd5641e36fb066c8796000ab886153
Prayag Verma <prayagpverma@gmail.com>
2024-05-03 01:16:19 -0500

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,13 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_fEkCkjUfx', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3de41o2bk1t94633ogottkiv14', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1"new_config"' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://e1l8s3d0bf.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
+
+// us-east-1_fEkCkjUfx
+// 3de41o2bk1t94633ogottkiv14
